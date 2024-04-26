6cd7327e4f7983d858e7d376b2ab9e4d89c00aad
shiv <shivpatel3@kpmg.com>
2024-04-26 13:10:11 +0530

    registration component and page

--- core/src/main/java/com/adobe/aem/guides/wknd/core/servlet/UserRegistrationServlet.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/servlet/UserRegistrationServlet.java
@@ -1,5 +1,9 @@
 package com.adobe.aem.guides.wknd.core.servlet;
 
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.Group;
+import org.apache.jackrabbit.api.security.user.User;
+import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.LoginException;
@@ -11,20 +15,21 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.Servlet;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import javax.jcr.*;
+import javax.servlet.Servlet;
 
 @Component(service = {Servlet.class}, property = {
         "sling.servlet.methods=GET",
-        "sling.servlet.paths=/bin/user-registration"
-}
-)
+        "sling.servlet.paths=/bin/user-registration"})
 public class UserRegistrationServlet extends SlingSafeMethodsServlet {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserRegistrationServlet.class);
     private static final String SYSTEM_SUB_SERVICE = "wknd-sub-service";
+    private static final String CONTENT_AUTHORS = "content-authors";
+    boolean creationFlag = true;
     @Reference
     private transient ResourceResolverFactory resourceResolverFactory;
 
@@ -37,14 +42,62 @@ public class UserRegistrationServlet extends SlingSafeMethodsServlet {
 
         try (ResourceResolver resourceResolver = resourceResolverFactory.getServiceResourceResolver(authInfo)) {
 
-            String userName = request.getParameter("userName");
+            LOGGER.info("Resource Resolver in Registration Servlet {}", resourceResolver);
+
+            //Getting session from resource resolver
+            Session session = resourceResolver.adaptTo(Session.class);
+
+            //Reading the parameter from servlet request
+            String userFirstName = request.getParameter("userFirstName");
+            String userLastName = request.getParameter("userLastName");
+            String userEmail = request.getParameter("userEmail");
+            String userId = request.getParameter("userId");
+            String userPassword = request.getParameter("userPassword");
+
+            //Checking if the User already exists
+            UserManager userManager = resourceResolver.adaptTo(UserManager.class);
+            assert userManager != null;
+            Authorizable authorizable = userManager.getAuthorizable(userId);
+
+            if (authorizable != null) {
+                // User exist set user creation flag as false
+                creationFlag = false;
+            } else {
+                //Creation of a new user with userID
+                User createdUser = userManager.createUser(userId, userPassword);
+
+                //Setting the createdUser Profile Property
+                assert session != null;
+                ValueFactory valueFactory = session.getValueFactory();
+                Value firstNameValue = valueFactory.createValue(userFirstName, PropertyType.STRING);
+                createdUser.setProperty("./profile/givenName", firstNameValue);
+
+                Value lastNameValue = valueFactory.createValue(userLastName, PropertyType.STRING);
+                createdUser.setProperty("./profile/familyName", lastNameValue);
+
+                Value emailValue = valueFactory.createValue(userEmail, PropertyType.STRING);
+                createdUser.setProperty("./profile/email", emailValue);
+
+                LOGGER.info("User successfully created with ID : {}", createdUser.getID());
+
+                //Addition of User to content-authors group
+                Group group = (Group) userManager.getAuthorizable(CONTENT_AUTHORS);
+                if (group != null) {
+                    Authorizable authorizeUser = userManager.getAuthorizable(createdUser.getID());
+                    assert authorizeUser != null;
+                    group.addMember(authorizeUser);
+                }
+
+                //saving the session
+                session.save();
+            }
 
             //Response writer
             response.setContentType("application/json");
             response.setCharacterEncoding("utf-8");
-            response.getWriter().write("Resource Resolver :" + resourceResolver.toString() + "User Name " + userName);
+            response.getWriter().write("User created successfully with UserId :" + userId);
 
-        } catch (LoginException | IOException e) {
+        } catch (LoginException | IOException | RepositoryException e) {
             LOGGER.error("Login exception for service: {}", e.getMessage());
         }
     }
